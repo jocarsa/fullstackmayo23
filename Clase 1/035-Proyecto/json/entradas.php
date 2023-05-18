603308395800320f98f3b07a1c2eb5647ae29579
jocarsa <info@josevicentecarratala.com>
2023-05-18 20:02:01 +0200

    Final clase 1

---
+++ Clase 1/035-Proyecto/json/entradas.php
@@ -0,0 +1,19 @@
+<?php
+// Me conecto a la base de datos
+$db = new mysqli("localhost", "aplicacion", "aplicacion", "aplicacion");
+// Creo un array vacío
+$jsonArray = [];
+// Preparo una petición
+$peticion = "SELECT * FROM entradas";
+// La lanzo a la base de datos
+$results = $db->query($peticion);
+// REcupero los resultados línea a línea
+while ($row = $results->fetch_array(MYSQLI_ASSOC)) {
+    // Los añado al array
+    $jsonArray[] = $row;
+}
+// Lo convierto y devuelvo en formato JSON
+echo json_encode($jsonArray);
+   
+   
+?>
---
+++ Clase 1/037-Proyecto/paneldecontrol.php
@@ -0,0 +1,68 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['llave'])){die("Te has intentado colar");}
+?>
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>Panel de control</title>
+        <meta charset="utf-8">
+        <link rel="Stylesheet" href="estilo/paneldecontrol.css">
+    </head>
+    <body>
+        <header>
+            <img src="img/logo.png" id="logo">
+            <h1>Panel de control</h1>
+        </header>
+        <nav>
+            <ul>
+                <li>Entradas</li>
+            </ul>
+        </nav>
+        <section>
+            <h2>Entradas</h2><a href=""><button id="botonnuevo">Nuevo</button></a>
+            <div class="caja">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Identificador</th>
+                            <th>Imagen</th>
+                            <th>Fecha</th>
+                            <th>Texto</th>
+                            <th>Titulo</th>
+                            <th>Usuario</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $mysqli = mysqli_connect(
+                                "localhost", 
+                                "aplicacion", 
+                                "aplicacion", 
+                                "aplicacion"
+                            );
+                            $peticion = "
+                                SELECT 
+                                * 
+                                FROM 
+                                entradas";
+                            $resultado = mysqli_query($mysqli, $peticion);
+                            while ($fila = mysqli_fetch_assoc($resultado)) {
+                                echo '
+                                    <tr>
+                                        <td>'.$fila['Identificador'].'</td>
+                                        <td>'.$fila['imagen'].'</td>
+                                        <td>'.$fila['fecha'].'</td>
+                                        <td>'.$fila['texto'].'</td>
+                                        <td>'.$fila['titulo'].'</td>
+                                        <td>'.$fila['FK_usuarios_nombrereal'].'</td>
+                                    </tr>
+                                    ';
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </body>
+</html>
